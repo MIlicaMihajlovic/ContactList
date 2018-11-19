2860883f695dc7688b3405da538a16b3411aa032
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-19 21:47:09 +0100

    Add form

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
+        <h3>Add contact</h3>
+
+         <div>
+             <!-- submit.prevent sprecava da se relouduje stranica nakon submit dugmeta kad klikne user -->
+            <form @submit.prevent="addContact">
+                <label>First Name</label>
+                <input v-model="newContact.firstName" placeholder="First Name"/><br>
+                <p>{{ firstName }}</p>
+
+                <label>Last Name</label>
+                <input v-model="newContact.lasttName" placeholder="Last Name"/><br>
+                <p>{{ lastName }}</p>
+
+                <label>Email</label>
+                <input v-model="newContact.email" placeholder="Email"/><br>
+                <p>{{ email }}</p>
+
+                <button type="submit">ADD CONTACT</button>
+            </form>
+
+        </div>
+        
         <h3>Contact List</h3>
+
         <table border=1>
             <thead>
                 <th>KEY</th>
@@ -17,6 +40,7 @@
                 </tr>
             </tbody>
         </table>
+       
     </div>
 </template>
 
@@ -24,15 +48,31 @@
 export default {
     data() {
         return {
+            newContact: {
+                firstName: '',
+                lastName: '',
+                email: '',
+            },
             contacts: [
                 {firstName: 'John', lastName: 'Doe', email: 'example@example.com'},
                 {firstName: 'Jack', lastName: 'Doe', email: 'example@example.com'},
                 {firstName: 'Ann', lastName: 'Doe', email: 'example@example.com'},
                 {firstName: 'Jasmin', lastName: 'Doe', email: 'example@example.com'},
                 {firstName: 'Susan', lastName: 'Doe', email: 'example@example.com'}
-            ]
+            ],
+
         };
+    },
+
+    methods: {
+        addContact() {
+            this.contacts.push(this.newContact);
+            //dodali smo u listu novokreirani 
+            this.newContact = {}
+            //ocistili smo objekat i bude prazan 
+        }
     }
 };
+
 </script>
 
